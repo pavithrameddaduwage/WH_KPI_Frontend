9b4da1c93c5acdd38074c7316d82d8f087dbe5ff
pavithrameddaduwage <pavithrameddaduwage@gmail.com>
2025-07-14 08:31:39 -0400

    home_changes+++++++++++++++++

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(path: string) {
+    this.router.navigate(['/' + path]);
+  }
 }
