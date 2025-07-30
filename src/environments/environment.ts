5cd5bd317f69168f5380cadd41174d602a2bf65b
pavithrameddaduwage <pavithrameddaduwage@gmail.com>
2025-07-30 08:41:29 -0400

    changes++++++++++++

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  uploadApiUrl: 'http://localhost:3800/upload',
-  authApiUrl: 'http://localhost:3800/auth/login',
+  uploadApiUrl: 'http://localhost:4005/upload',
+  authApiUrl: 'http://localhost:4005/auth/login',
 
 };
