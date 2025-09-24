b3c0e1dd5b5d30f27bf21918b1c8d881895e583a
pavithrameddaduwage <pavithrameddaduwage@gmail.com>
2025-09-24 06:58:17 -0400

    changes++++++++++

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 const backendPort = 4012;
 const backendBaseUrl = `http://localhost:${backendPort}`;
-// const backendBaseUrl='https://hbs.hgusa.com/api/wh-report';
-// const productionUrl = 'https://hbs.hgusa.com/wh-report/upload';
+// const backendBaseUrl='https://hbs.hgusa.com/api/kpi-upload';
+
 
 export const environment = {
 
